0d9b0771325646182668170d6b828a340d610e45
Al <alexunder_5542@mail.ru>
2021-09-14 18:29:54 +0300

    Initial Commit

---
+++ Finan/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Finan
+//
+//  Created by tambanco 🥳 on 14.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
