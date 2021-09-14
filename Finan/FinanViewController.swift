4fd28f2a8557d9fadb1e2cdbc053fb6ba58b3672
Al <alexunder_5542@mail.ru>
2021-09-14 19:55:49 +0300

    added stack Views

---
+++ Finan/FinanViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FinanViewController.swift
+//  Finan
+//
+//  Created by tambanco 🥳 on 14.09.2021.
+//
+
+import UIKit
+
+class FinanViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
